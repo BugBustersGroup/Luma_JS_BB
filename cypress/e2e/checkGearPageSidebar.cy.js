45e9ec2d31c6f1bd344143cd8342903e6aac97c5
Vladimir Vakulenko <vakulenko.volodia@yandex.ru>
2024-04-07 02:45:40 +0300

    commit 07.04.2024

--- cypress/e2e/checkGearPageSidebar.cy.js
+++ cypress/e2e/checkGearPageSidebar.cy.js
@@ -3,35 +3,71 @@
 import GearPage from "../pageObjects/GearPage.js";
 import gearPageData from "../fixtures/gearPageData.json";
 
-describe('checkGearPageSidebar', () => {
-    const gearPage = new GearPage();
+describe("checkGearPageSidebar", () => {
+  const gearPage = new GearPage();
 
-    beforeEach(() => {
-        cy.visit(gearPageData.ExampleUrl);
+  beforeEach(() => {
+    cy.visit(gearPageData.ExampleUrl);
+  });
+
+  it("TC_009.001.001 | Gear page > Shop by category > Visibility of sidebar title", () => {
+    gearPage
+      .getFirstStringTitle()
+      .should("contain", gearPageData.FirstStringTitle)
+      .and("be.visible");
+    gearPage
+      .getSecondStringTitle()
+      .should("contain", gearPageData.SecondStringTitle)
+      .and("be.visible");
+  });
+
+  gearPageData.dropDownMenu.forEach((item, index) => {
+    it("TC_009.001.001 | Gear page > Shop by category > Visibility of categories", () => {
+      gearPage.clickCategoryMenuTitles(item, index);
+      cy.contains(gearPageData.categoryMenu[index]);
     });
+  });
 
-    it('TC_009.001.001 | Gear page > Shop by category > Visibility of categories', () => {
+  it("TC_009.002.001 | Gear page > Shop by category > Quantity of products", function () {
+    gearPage
+      .getBagsCategoryQuantity()
+      .invoke("text")
+      .then((text1) => {
+        gearPage.getBagsLink().click();
         gearPage
-            .getFirstStringTitle()
-            .should('contain', gearPageData.FirstStringTitle)
-            .and('be.visible')
-        gearPage
-            .getSecondStringTitle()
-            .should('contain', gearPageData.SecondStringTitle)
-            .and('be.visible')    
-        gearPage
-            .getBagsCategory()
-            .should('contain', gearPageData.FirstCategory)
-            .and('be.visible')
-        gearPage
-            .getFitnessEquipmentCategory()
-            .should('contain', gearPageData.SecondCategory)
-            .and('be.visible')
-        gearPage
-            .getWatchesCategory()
-            .should('contain', gearPageData.ThirdCategory)
-            .and('be.visible')
+          .getItemsQuantity()
+          .invoke("text")
+          .then((text2) => {
+            expect(text1).equal(text2);
+          });
+        gearPage.clickBackToGearPage();
+      });
 
-    });
+    gearPage
+      .getFitnessEquipmentCategoryQuantity()
+      .invoke("text")
+      .then((text1) => {
+        gearPage.getFitnessEquipmentLink().click();
+        gearPage
+          .getItemsQuantity()
+          .invoke("text")
+          .then((text2) => {
+            expect(text1).equal(text2);
+          });
+        gearPage.clickBackToGearPage();
+      });
+
+    gearPage
+      .getWatchesCategoryQuantity()
+      .invoke("text")
+      .then((text1) => {
+        gearPage.getWatchesLink().click();
+        gearPage
+          .getItemsQuantity()
+          .invoke("text")
+          .then((text2) => {
+            expect(text1).equal(text2);
+          });
+      });
+  });
 });
-
--- cypress/pageObjects/GearPage.js
+++ cypress/pageObjects/GearPage.js
@@ -1,9 +1,32 @@
 class GearPage {
-    getFirstStringTitle = () => cy.get('div.title:first-of-type strong');
-    getSecondStringTitle = () => cy.get('#narrow-by-list2 dt');
-    getBagsCategory = () => cy.get('ol.items li:first-child');
-    getFitnessEquipmentCategory = () => cy.get('ol.items li:nth-child(2)');
-    getWatchesCategory = () => cy.get('ol.items li:last-child');
+  getGearDropDownMenuTitles = () => cy.get(".nav-4 ul span");
+  getFirstStringTitle = () => cy.get("div.title:first-of-type strong");
+  getSecondStringTitle = () => cy.get("#narrow-by-list2 dt");
+  getBagsCategoryQuantity = () => cy.get("ol.items li:first-child span");
+  getFitnessEquipmentCategoryQuantity = () =>
+    cy.get("ol.items li:nth-child(2) span");
+  getWatchesCategoryQuantity = () => cy.get("ol.items li:last-child span");
+  getBagsLink = () => cy.get(".columns dd li:first-child a");
+  getFitnessEquipmentLink = () => cy.get(".columns dd li:nth-child(2) a");
+  getWatchesLink = () => cy.get(".columns dd li:last-child a");
+  getCategoryMenuTitles = () => cy.get("dl#narrow-by-list2 a");
+  getBackToGearPage = () => cy.get(".breadcrumbs li:nth-child(2) a");
+  getItemsQuantity = () =>
+    cy.get("#authenticationPopup+div p.toolbar-amount span:last-child");
+
+  clickCategoryMenuTitles(itemName, index) {
+    this.getCategoryMenuTitles().eq(index).as("item");
+    cy.get("@item").contains(itemName);
+    cy.get("@item").click();
+
+    return this;
+  }
+
+  clickBackToGearPage() {
+    this.getBackToGearPage().click();
+
+    return this;
+  }
 }
 
 export default GearPage;
